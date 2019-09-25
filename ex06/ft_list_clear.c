55efefc9f5d7e6a4989899acc4cc828cb60445c1
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-25 10:12:17 +0200

    ex06 now cleaner and without bugs

--- ex06/ft_list_clear.c
+++ ex06/ft_list_clear.c
@@ -6,32 +6,64 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/24 14:44:59 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/24 15:13:14 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/25 10:10:10 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
+t_list	*ft_list_last(t_list *begin_list)
+{
+	while ((*begin_list).next)
+		begin_list = (*begin_list).next;
+	return (begin_list);
+}
+
+int		ft_list_size(t_list *begin_list)
+{
+	int		i;
+
+	i = 0;
+	while (begin_list)
+	{
+		begin_list = (*begin_list).next;
+		i++;
+	}
+	return (i);
+}
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	nbr--;
+	while (begin_list && nbr)
+	{
+		begin_list = (*begin_list).next;
+		nbr--;
+	}
+	return (begin_list);
+}
+
 void	ft_list_clear(t_list **begin_list)
 {
 	t_list	*last;
 	t_list	*second;
+	int		length;
 
-	second = (*begin_list);
-	while (second)
+	last = ft_list_last(*begin_list);
+	length = 2;
+	while (length > 1)
 	{
-		second = (*begin_list);
-		while ((*second).next && (*second).next->next)
-			second = (*second).next;
-		last = (*second).next;
-		(*second).next = 0;
-		if (last)
-			free(last);
+		last = ft_list_last(*begin_list);
+		length = ft_list_size(*begin_list);
+		if (length > 1)
+		{
+			second = ft_list_at(*begin_list, length - 1);
+			(*second).next = 0;
+		}
 		else
 		{
-			free(second);
-			break ;
+			*begin_list = 0;
 		}
+		free(last);
 	}
-	*begin_list = 0;
 }
